e404476945678b99969693cacfe16a73ef927753
marisa <marisa.petrusky@gmail.com>
2019-06-12 13:41:32 -0400

    Fixed bug

--- GEM_Efficiency.C
+++ GEM_Efficiency.C
@@ -1,62 +1,75 @@
-bool res_bad(double x, double res)
-{
-	if (x < 0)
-	{x = -1*x;}
-	if (x > res)
+bool isBadTrack(double x)
+{	
+	double resolution = 1e-3;
+	if (fabs(x) > resolution) 
 	{return true;}
-	else
-	{return false;}		
+	else 
+	{return false;}
+}
+
+void PlaneCheck(int iplane)
+{
+	// User Variables
+	bool xdummy, ydummy;
+	// Branch Variables
+	double tr;
+	double x_resid;
+	double y_resid;
+	long entries;
+	// Calculated Variables
+	double evt_proj = 0;	
+	double evt_bad = 0;
+	double evtx_bad = 0;
+	double evty_bad = 0;
+	double efficiency = 0;
+	
+	TString filename = Form("/home/marisa/rootfiles/test_417_plane%d.root",iplane);
+	TFile * p_infile = TFile::Open(filename,"READ");
+	TTree * t_T = (TTree*)p_infile->Get("T");
+	entries = t_T->GetEntries();
+	
+	t_T->SetBranchAddress("prex.tr.n",&tr);
+	TString xbranch = Form("prex.gems.x%d.coord.resid",iplane);
+	t_T->SetBranchAddress(xbranch,&x_resid);
+	TString ybranch = Form("prex.gems.y%d.coord.resid",iplane);
+	t_T->SetBranchAddress(ybranch,&y_resid);
+
+	for (int ientry = 0; ientry < entries; ientry++)	
+	{
+		t_T->GetEntry(ientry);
+		
+		if (tr > 0)
+		{
+			evt_proj++;
+			xdummy = isBadTrack(x_resid);
+			ydummy = isBadTrack(y_resid);
+			if (xdummy)
+			{evtx_bad++;}
+			if (ydummy)
+			{evty_bad++; cout << "Found bad y" << endl;}
+		}
+	}
+		
+	if (evtx_bad > evty_bad)
+	{evt_bad = evtx_bad;}
+	else 
+	{evt_bad = evty_bad;}
+	
+	cout << "Projected Events: " << evt_proj << endl;
+	cout << "Bad Events: " << evt_bad << endl;
+	efficiency = 1 - evt_bad/evt_proj;
+	
+	cout << "GEM Efficiency = " << efficiency << endl;
+	p_infile->Close();
 }
 
 void GEM_Efficiency()
 {
-	// User Variables
-	double resolution = 0.001;
-	// Branch Variables
-	double tr; 
-	double x0_resid = 0, x1_resid = 0, x2_resid = 0, x3_resid = 0;
-	long entries;
-	// Calculated Variables
-	double evt_proj[4] = {0,0,0,0}, evt_verf[4] = {0,0,0,0};
-	double efficiency[4] = {0,0,0,0};
-	
-	for (int iplane = 0; iplane < 4; iplane++)
+	int planes = 4;
+	for (int iplane = 1; iplane <= planes; iplane++)
 	{
-		TString filename = Form("/home/marisa/rootfiles/test_416_plane%d.root",iplane);
-		TFile * p_infile = TFile::Open(filename,"READ");
-		TTree * t_T = (TTree*)p_infile->Get("T");
-		entries = 5000;//t_T->GetEntries();
-		
-		t_T->SetBranchAddress("prex.tr.n",&tr);
-		t_T->SetBranchAddress("prex.gems.x1.coord.resid",&x0_resid);
-		t_T->SetBranchAddress("prex.gems.x2.coord.resid",&x1_resid);
-		t_T->SetBranchAddress("prex.gems.x3.coord.resid",&x2_resid);
-		t_T->SetBranchAddress("prex.gems.x4.coord.resid",&x3_resid);
-	
-		for (int ientry = 0; ientry < entries; ientry++)
-		{
-			t_T->GetEntry(ientry);
-			if (tr > 0)
-			{
-				evt_proj[iplane]++;
-				if (iplane == 0 && res_bad(x0_resid,resolution))
-				{	//cout << "x0_resid = " << x0_resid << endl;
-					evt_verf[0] = evt_verf[0] + 1;}
-				if (iplane == 1 && res_bad(x1_resid,resolution))
-				{	//cout << "x1_resid = " << x1_resid << endl;
-					evt_verf[1] = evt_verf[1] + 1;}
-				if (iplane == 2 && res_bad(x2_resid,resolution))
-				{	//cout << "x2_resid = " << x2_resid << endl;
-					evt_verf[2] = evt_verf[2] + 1;}
-				if (iplane == 3 && res_bad(x3_resid,resolution))
-				{	//cout << "x3_resid = " << x3_resid << endl;
-					evt_verf[3] = evt_verf[3] + 1;}
-			}	
-		}
-		cout << "Projected Events: " << evt_proj[iplane] << endl;
-		cout << "Bad Events: " << evt_verf[iplane] << endl;
-		efficiency[iplane] = 1 - evt_verf[iplane]/evt_proj[iplane];
-		cout << "GEM" << iplane << " Efficiency: " << efficiency[iplane] << endl;
-		p_infile->Close();
+		cout << "Checking GEM" << iplane << "..." << endl;
+		PlaneCheck(iplane);
 	}
+	
 }
