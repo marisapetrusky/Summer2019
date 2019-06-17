4b17b1ffd0fcc4561bff2bd73d01e7ba3ca56a17
marisapetrusky <marisapetrusky@gmail.com>
2019-06-17 10:31:53 -0400

    commit

--- QuartzMap_TH3D.C
+++ QuartzMap_TH3D.C
@@ -1,7 +1,8 @@
 void QuartzMap_TH3D()
 {
 	// User Variables
-	int run;
+	int runs[9] = {427,430,431,438,439,440,441,442,444};
+	int noruns = 9;
 	// Branch Variables
 	double x1, x2, x3, x4;
 	double y1, y2, y3, y4;
@@ -27,44 +28,49 @@ void QuartzMap_TH3D()
 	h_q2_h->GetXaxis()->SetTitle("X-axis (5 cm long)");
 	h_q2_h->GetYaxis()->SetTitle("Y-axis (10 cm long)");
 
-	cout << "Enter run number" << endl;
-	cin >> run;
+	//cout << "Enter run number" << endl;
+	//cin >> run;
 
-	TString s_infile = Form("/home/marisa/rootfiles/test_%d_verf.root",run);
-	TFile * p_infile = TFile::Open(s_infile,"READ");
-	TTree * t_T = (TTree*)p_infile->Get("T");
-	entries = t_T->GetEntries();
-	t_T->SetBranchAddress("x1_coord",&x1);
-	t_T->SetBranchAddress("x2_coord",&x2);
-	t_T->SetBranchAddress("x3_coord",&x3);
-	t_T->SetBranchAddress("x4_coord",&x4);
-	t_T->SetBranchAddress("y1_coord",&y1);
-	t_T->SetBranchAddress("y2_coord",&y2);
-	t_T->SetBranchAddress("y3_coord",&y3);
-	t_T->SetBranchAddress("y4_coord",&y4);
-	t_T->SetBranchAddress("qx1_coord",&qx1);
-	t_T->SetBranchAddress("qx2_coord",&qx2);
-	t_T->SetBranchAddress("qy1_coord",&qy1);
-	t_T->SetBranchAddress("qy2_coord",&qy2);
-	t_T->SetBranchAddress("phi",&ph);
-	t_T->SetBranchAddress("theta",&th);
-	t_T->SetBranchAddress("h_adc0",&hadc0);
-	t_T->SetBranchAddress("h_adc1",&hadc1);
-	t_T->SetBranchAddress("l_adc0",&ladc0);
-	t_T->SetBranchAddress("l_adc1",&ladc1);
-	
-	for (int ievt = 0; ievt < entries; ievt++)
+	for (int irun = 0; irun < noruns; irun++)
 	{
-		t_T->GetEntry(ievt);
-		//cout << qx1 << endl;
-		//if (ladc0 > pedestal_l[0] && ladc1 > pedestal_l[1] && hadc0 > pedestal_h[0] && hadc1 > pedestal_h[1])
+		cout << "Processing Run " << runs[irun] << endl;
+
+		TString s_infile = Form("/home/marisa/rootfiles/test_%d_verf.root",runs[irun]);
+		TFile * p_infile = TFile::Open(s_infile,"READ");
+		TTree * t_T = (TTree*)p_infile->Get("T");
+		entries = t_T->GetEntries();
+		t_T->SetBranchAddress("x1_coord",&x1);
+		t_T->SetBranchAddress("x2_coord",&x2);
+		t_T->SetBranchAddress("x3_coord",&x3);
+		t_T->SetBranchAddress("x4_coord",&x4);
+		t_T->SetBranchAddress("y1_coord",&y1);
+		t_T->SetBranchAddress("y2_coord",&y2);
+		t_T->SetBranchAddress("y3_coord",&y3);
+		t_T->SetBranchAddress("y4_coord",&y4);
+		t_T->SetBranchAddress("qx1_coord",&qx1);
+		t_T->SetBranchAddress("qx2_coord",&qx2);
+		t_T->SetBranchAddress("qy1_coord",&qy1);
+		t_T->SetBranchAddress("qy2_coord",&qy2);
+		t_T->SetBranchAddress("phi",&ph);
+		t_T->SetBranchAddress("theta",&th);
+		t_T->SetBranchAddress("h_adc0",&hadc0);
+		t_T->SetBranchAddress("h_adc1",&hadc1);
+		t_T->SetBranchAddress("l_adc0",&ladc0);
+		t_T->SetBranchAddress("l_adc1",&ladc1);
+
+		for (int ievt = 0; ievt < entries; ievt++)
 		{
-			h_q1_l->Fill(qx1,qy1,ladc0);
-			h_q2_l->Fill(qx2,qy2,ladc1);
-			h_q1_h->Fill(qx1,qy1,hadc0);
-			h_q2_h->Fill(qx2,qy2,hadc1);
-		}
-	}	
-	
+			t_T->GetEntry(ievt);
+			//cout << qx1 << endl;
+			if (ladc0 > pedestal_l[0] && ladc1 > pedestal_l[1] && hadc0 > pedestal_h[0] && hadc1 > pedestal_h[1])
+			{
+				h_q1_l->Fill(qx1,qy1,ladc0);
+				h_q2_l->Fill(qx2,qy2,ladc1);
+				h_q1_h->Fill(qx1,qy1,hadc0);
+				h_q2_h->Fill(qx2,qy2,hadc1);
+			}
+		}	
+	}
+
 	h_q1_l->Draw();	
 }
