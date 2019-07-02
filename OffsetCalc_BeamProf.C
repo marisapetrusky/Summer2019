e473d56940b179d0b0d0fb78e04bd50af34ca994
Hall A Online <a-onl@jlab.org>
2019-07-02 19:22:17 -0400

    Plot edits

--- OffsetCalc_BeamProf.C
+++ OffsetCalc_BeamProf.C
@@ -11,13 +11,18 @@ void OffsetCalc_BeamProf()
 	auto t_in = f_in->Get("T");
 	RDataFrame d("T",f_in);
 	
+	TChain * ch = new TChain("T");
+	ch->Add(s_in);
+	TH2D * h_g1 = new TH2D("test","test",512,-0.1,0.1,256,-0.05,0.05);
+	ch->Draw("x1_coordt:x2_coordt >> test");
 	//auto h_g1 = d.Histo2D<double,double>({"h_g1","Beam Profile, GEM2",512,-0.1,0.1,256,-0.05,0.05},"x1_coordt","y1_coordt"); 	
-	auto h_g1 = d.Histo2D<double,double>({},"x1_coordt","y1_coordt"); 	
+	//auto h_g1 = d.Histo2D<double,double>({},"x1_coordt","y1_coordt"); 	
 	//auto h_g2 = d.Histo2D({"h_g2","Beam Profile, GEM2",256u,-0.05,0.05,512u,-0.1,0.1},"x2_coordt","y2_coordt"); 	
 	//auto h_g3 = d.Histo2D({"h_g3","Beam Profile, GEM3",256u,-0.05,0.05,512u,-0.1,0.1},"x3_coordt","y3_coordt");
 	
+	
 	//TCanvas * c1 = new TCanvas("c1","c1",800,600);
-	h_g1->Draw();
+	//h_g1->Draw();
 	//TCanvas * c2 = new TCanvas("c2","c2",800,600);
 	//h_g2->Draw();
 	//TCanvas * c3 = new TCanvas("c3","c3",800,600);
